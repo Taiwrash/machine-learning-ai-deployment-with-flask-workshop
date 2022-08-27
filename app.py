c06ef6fcde8ad6794953f859de9b15e7ac6c8bc1
mudasiru rasheed <rasheedrtm1@gmail.com>
2022-08-27 06:57:33 +0100

    heroku deployment

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def result():
         print(prediction)
     return render_template("predict.html",prediction=prediction)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+##importing the app from main file
+from app import app
+if __name__ == "__main__": 
+    app.run()
